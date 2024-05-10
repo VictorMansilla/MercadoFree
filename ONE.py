ba3fdf483530da41e1c8e8cb2c316b085e9aa758
Victor Mansilla <Victormansilla.vrm@gmail.com>
2024-05-09 21:14:26 -0300

    Agregando el la función de editar los productos 'Tomó mucho tiempo jaja'

--- ONE.py
+++ ONE.py
@@ -3,8 +3,7 @@ from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from TWO import *
-from typing import Annotated, Union
-#from pydantic import BaseModel
+from typing import Annotated
 
 app_de_FastApi = FastAPI()
 app_de_FastApi.mount("/static",StaticFiles(directory="static"),name="static")
@@ -15,20 +14,25 @@ Ninja=Jinja2Templates(directory="Templates")
 def root(request:Request):
     return Ninja.TemplateResponse("index.html",{"request":request})
 
-@app_de_FastApi.get("/dashboard",response_class=HTMLResponse)
-def Dashboard(request:Request):
-    return Ninja.TemplateResponse("dashboard.html", {"request": request})
-
-@app_de_FastApi.post("/Productoss", response_class=RedirectResponse)
-async def Ingreso_Producto(Nombre_Producto:Annotated[str,Form()],Precio_Producto:Annotated[int,Form()],Descripcion_Producto:Annotated[str,Form()]):
-    Agreagar_Producto(Nombre_Producto,Precio_Producto,Descripcion_Producto)
-    return RedirectResponse("/Productos", status_code=303)
-
 @app_de_FastApi.get("/Productos",response_class=HTMLResponse)
 def Mostrar_Productos(request:Request):
     return Ninja.TemplateResponse("Productos.html",{"request":request, "Datos":Printear_Todos_los_Datos_del_DB()})
 
-@app_de_FastApi.delete("/Producto_Eliminar", response_class=RedirectResponse)
+@app_de_FastApi.post("/Productoss", response_class=RedirectResponse)
+def Ingreso_Producto(Nombre_Producto:Annotated[str,Form()],Precio_Producto:Annotated[int,Form()],Descripcion_Producto:Annotated[str,Form()]):
+    Agreagar_Producto(Nombre_Producto,Precio_Producto,Descripcion_Producto)
+    return RedirectResponse("/Productos", status_code=303)
+
+@app_de_FastApi.get("/Producto_Eliminar", response_class=RedirectResponse)
 def Producto_Eliminar(ID:int):
     Borrar_Producto(ID)
+    return RedirectResponse("/Productos", status_code=303)
+
+@app_de_FastApi.get("/Editar_Producto", response_class=HTMLResponse)
+def Editar_Producto_(ID, request:Request):
+    return Ninja.TemplateResponse("Editar_Producto.html",{"request":request, "ID":ID})
+
+@app_de_FastApi.post("/Producto_editado", response_class=RedirectResponse)
+def Producto_Editado(ID:Annotated[int,Form()], Nombre_Editado:Annotated[str,Form()],Precio_Editado:Annotated[int,Form()],Descripcion_Editada:Annotated[str,Form()]):
+    Editar_Producto(ID, Nombre_Editado, Precio_Editado, Descripcion_Editada)
     return RedirectResponse("/Productos", status_code=303)
--- TWO.py
+++ TWO.py
@@ -1,5 +1,4 @@
 import sqlite3
-from fastapi import HTTPException
 
 Creation_Base_Data = ('''CREATE TABLE IF NOT EXISTS Productos(
                          ID INTEGER,
@@ -9,11 +8,11 @@ Creation_Base_Data = ('''CREATE TABLE IF NOT EXISTS Productos(
                          PRIMARY KEY(ID AUTOINCREMENT)
 )''')
 
-class producto:
+""" class producto:
     def __init__(self,Nombre,Precio,Descripcion):
         self.Nombre = Nombre
         self.Precio = Precio
-        self.Descripcion = Descripcion
+        self.Descripcion = Descripcion """
 
 class Coneaxion_a_DB:
     def __init__(self):
@@ -30,23 +29,23 @@ def Printear_Todos_los_Datos_del_DB():
     DB.cursor.execute("SELECT * FROM Productos")
     return DB.cursor.fetchall()
 
-#Agreagar_Producto()
+#Agreagar un nuevo producto
 
 def Agreagar_Producto(Nombre_Producto,Precio_Producto,Descripcion_Producto):
     DB = Coneaxion_a_DB()
     DB.cursor.execute(f"""INSERT OR IGNORE INTO Productos (PRODUCTO_NOMBRE,PRECIO,DESCRIPCION) VALUES ('{Nombre_Producto}','{Precio_Producto}','{Descripcion_Producto}')""")
     DB.cerrar()
 
-#Borrar_Producto()
+#Borrar el producto seleccionado
 
 def Borrar_Producto(ID):
     DB = Coneaxion_a_DB()
     DB.cursor.execute(f'DELETE FROM Productos Where ID like "{ID}"')
     DB.cerrar()
 
-#Editar_Producto()
+#Editar un producto seleccionado
 
-def Editar_Producto(ID):
+def Editar_Producto(ID, Nombre_Editado, Precio_Editado, Descripcion_Editada):
     DB = Coneaxion_a_DB()
-    DB.cursor.execute(f'UPDATE FROM Productos Where PRECIO like "{ID}"')
+    DB.cursor.execute(f'UPDATE Productos SET PRODUCTO_NOMBRE="{Nombre_Editado}", PRECIO={Precio_Editado}, DESCRIPCION="{Descripcion_Editada}" WHERE ID="{ID}"')
     DB.cerrar()
