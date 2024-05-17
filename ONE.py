b23bbb4fe69ebe51ca355b198ce1f2c61a8e54d0
Victor Mansilla <Victormansilla.vrm@gmail.com>
2024-05-16 21:46:55 -0300

    Creación de la sección de 'Login' y 'Register' incluyendo la base de datos de usuarios

--- ONE.py
+++ ONE.py
@@ -3,13 +3,43 @@ from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from TWO import *
-from typing import Annotated, Optional
-
+from typing import Annotated
 
 app_de_FastApi = FastAPI()
 app_de_FastApi.mount("/static",StaticFiles(directory="static"),name="static")
 
 Ninja=Jinja2Templates(directory="Templates")
+
+#Registro de usuario ____________________________________________________________________________________________________________________________
+
+@app_de_FastApi.get("/Register", response_class=HTMLResponse)
+def Register(request:Request):
+    return Ninja.TemplateResponse("Register.html", {"request":request})
+
+@app_de_FastApi.post("/RegisterUser",response_class=RedirectResponse)
+def Register_User(UserName:Annotated[str,Form()], PassWord:Annotated[str,Form()], PassWord_Confirm:Annotated[str,Form()]):
+    if PassWord == PassWord_Confirm:
+        if Agreagar_Usuario(UserName, PassWord):
+            return RedirectResponse("/", status_code=303)
+        else:
+            return RedirectResponse("/Register", status_code=303)
+    else:
+        return RedirectResponse("/Register", status_code=303)
+
+#Logeo de usuario ____________________________________________________________________________________________________________________________
+
+@app_de_FastApi.get("/Login", response_class=HTMLResponse)
+def Login(request:Request):
+    return Ninja.TemplateResponse("Login.html", {"request":request})
+
+@app_de_FastApi.post("/LoginUser",response_class=RedirectResponse)
+def Login_User(UserName:Annotated[str,Form()], PassWord:Annotated[str,Form()]):
+    if Validar_Usuario(UserName, PassWord) == True:
+        return RedirectResponse("/", status_code=303)
+    else:
+        return RedirectResponse("/Login", status_code=303)
+
+#MercadoFree ____________________________________________________________________________________________________________________________
 
 @app_de_FastApi.get("/", response_class=HTMLResponse)
 def root(request:Request):
--- TWO.py
+++ TWO.py
@@ -1,58 +1,97 @@
 import sqlite3
 
+#Base de datos de productos
+
 Creation_Base_Data = ('''CREATE TABLE IF NOT EXISTS Productos(
                          ID INTEGER,
                          PRODUCTO_NOMBRE VARCHAR(25),
                          PRECIO INTEGER,
                          DESCRIPCION VARCHAR(500),
-                         PRIMARY KEY(ID AUTOINCREMENT)
-)''')
+                         PRIMARY KEY(ID AUTOINCREMENT))''')
 
-""" class producto:
-    def __init__(self,Nombre,Precio,Descripcion):
-        self.Nombre = Nombre
-        self.Precio = Precio
-        self.Descripcion = Descripcion """
+#Base de datos de usuarios
 
-class Coneaxion_a_DB:
+Creation_Base_Data_Usuarios = ('''CREATE TABLE IF NOT EXISTS Usuarios(
+                      ID INTEGER,
+                      USUARIO_NOMBRE VARCHAR(25),
+                      USUARIO_CONTRASEÑA VARCHAR(50),
+                      PRIMARY KEY(ID AUTOINCREMENT))''')
+
+class Coneaxion_a_DB_Productos:
     def __init__(self):
-        self.base_datos = 'C:\\Users\\User\\Desktop\\tarea\\PYtrabajo\\BANGA\\Base_Datos_Productos.db'
-        self.conexion = sqlite3.connect(self.base_datos)
+        self.base_datos_productos = 'C:\\Users\\User\\Desktop\\tarea\\PYtrabajo\\BANGA\\Base_Datos_Productos.db'
+        self.conexion = sqlite3.connect(self.base_datos_productos)
+        self.conexion.execute(Creation_Base_Data)
         self.cursor = self.conexion.cursor()
 
     def cerrar(self):
         self.conexion.commit()
         self.conexion.close()
 
+#____________________________________________________________ Sección de productos ____________________________________________________________
+
 def Printear_Todos_los_Datos_del_DB():
-    DB = Coneaxion_a_DB()
+    DB = Coneaxion_a_DB_Productos()
     DB.cursor.execute("SELECT * FROM Productos")
     return DB.cursor.fetchall()
 
 #Agreagar un nuevo producto
 
 def Agreagar_Producto(Nombre_Producto,Precio_Producto,Descripcion_Producto):
-    DB = Coneaxion_a_DB()
+    DB = Coneaxion_a_DB_Productos()
     DB.cursor.execute(f'INSERT OR IGNORE INTO Productos (PRODUCTO_NOMBRE,PRECIO,DESCRIPCION) VALUES ("{Nombre_Producto}","{Precio_Producto}","{Descripcion_Producto}")')
     DB.cerrar()
 
 #Borrar el producto seleccionado
 
 def Borrar_Producto(ID):
-    DB = Coneaxion_a_DB()
+    DB = Coneaxion_a_DB_Productos()
     DB.cursor.execute(f'DELETE FROM Productos Where ID like "{ID}"')
     DB.cerrar()
 
 #Editar un producto seleccionado
 
 def Editar_Producto(ID, Nombre_Editado, Precio_Editado, Descripcion_Editada):
-    DB = Coneaxion_a_DB()
+    DB = Coneaxion_a_DB_Productos()
     DB.cursor.execute(f'UPDATE Productos SET PRODUCTO_NOMBRE="{Nombre_Editado}", PRECIO={Precio_Editado}, DESCRIPCION="{Descripcion_Editada}" WHERE ID="{ID}"')
     DB.cerrar()
 
 #Buscar los productos
 
 def Buscar_Producto(Buscar_Producto_s):
-    DB = Coneaxion_a_DB()
+    DB = Coneaxion_a_DB_Productos()
     DB.cursor.execute(f"SELECT * FROM Productos WHERE PRODUCTO_NOMBRE LIKE '{Buscar_Producto_s}'")
     return DB.cursor.fetchall()
+
+#____________________________________________________________ Sección de usuarios ____________________________________________________________
+
+class Coneaxion_a_Usuarios(Coneaxion_a_DB_Productos):
+    def __init__(self):
+        self.base_datos_usuarios = 'C:\\Users\\User\\Desktop\\tarea\\PYtrabajo\\BANGA\\Base_Datos_Usuarios.db'
+        self.conexion = sqlite3.connect(self.base_datos_usuarios)
+        self.conexion.execute(Creation_Base_Data_Usuarios)
+        self.cursor = self.conexion.cursor()
+    
+    def cerrar():
+        return super().cerrar()
+    
+#Agregar un nuevo usuario (Registro)
+
+def Agreagar_Usuario(Nombre_Usuario, Contraseña_Usuario):
+    DB = Coneaxion_a_Usuarios()
+    DB.cursor.execute(f'SELECT 1 FROM Usuarios WHERE USUARIO_NOMBRE="{Nombre_Usuario}" LIMIT 1')
+    result = DB.cursor.fetchone()
+    if result == False:
+        DB.cursor.execute(f'INSERT OR IGNORE INTO Usuarios (USUARIO_NOMBRE,USUARIO_CONTRASEÑA) VALUES ("{Nombre_Usuario}","{Contraseña_Usuario}")')
+        DB.cerrar()
+        return True
+
+#Verificar usuario (Logeo)
+
+def Validar_Usuario(Nombre_Usuario, Contraseña_Usuario):
+    DB = Coneaxion_a_Usuarios()
+    DB.cursor.execute(f'SELECT * FROM Usuarios WHERE USUARIO_NOMBRE="{Nombre_Usuario}" AND USUARIO_CONTRASEÑA="{Contraseña_Usuario}"')
+    Verificar = DB.cursor.fetchall()
+    if Verificar:
+        return True
+    else:return False
